46f938f51300f339ce942dc9d1d9a8e4c8d7f12e
hieu2102 <gobanme.pierro@gmail.com>
2019-07-06 00:11:06 +0700

    add basic http auth

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const http = require('http'), parser = require('body-parser');
-var express = require('express'), app = express(), portApi = process.env.port || 80,fs = require('fs'), username, password;
+const http = require('http'),
+    parser = require('body-parser');
+var express = require('express'),
+    app = express(),
+    portApi = process.env.port || 80,
+    fs = require('fs'),
+    username, password;
 app.use(parser.urlencoded({
     extended: true
 }));
@@ -11,36 +16,36 @@ app.use(parser.json());
 app.listen(portApi);
 
 // getCredential();
-app.post('/read', (req, res)=>{
+app.post('/read', (req, res) => {
     console.log(req.body);
     var inputUsername = req.body.username;
     var inputPassword = req.body.password;
     var storedUsername = fs.readFileSync('store.txt');
 });
 
-app.post('/', (req, res)=>{
+app.post('/', (req, res) => {
     console.log(req.headers.authorization);
     res.status(200).send('abc');
 });
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     // console.log(req);
     // res.send("avc");
     getCredential();
     console.log(JSON.stringify(req.headers.authorization));
     var date = new Date();
     res.status(200).json({
-        "username": username, 
-        "password":password,
-        "day":date.getDate(),
+        "username": username,
+        "password": password,
+        "day": date.getDate(),
         "hr": date.getUTCHours(),
         "yr": date.getFullYear(),
-        'fdate':date.getTime()
+        'fdate': date.getTime()
 
-});
+    });
     // res.send(getCredential());
 });
 
-function getCredential(){
+function getCredential() {
     var data = fs.readFileSync('store.json');
     let cred = JSON.parse(data);
     username = cred.username;
@@ -48,28 +53,52 @@ function getCredential(){
 }
 
 
-app.post('/auth/jwt', (req, res)=>{
+app.post('/auth/jwt', (req, res) => {
     console.log(req.body);
     var inputUsername = req.body.username;
     var inputPassword = req.body.password;
     getCredential();
-    if (inputUsername.trim() == username && inputPassword.trim() == password){
+    if (inputUsername.trim() == username && inputPassword.trim() == password) {
         var date = new Date();
-        res.status(200).json({"tokem":"timeStamp_"+date.getTime()});
+        res.status(200).json({
+            "tokem": "timeStamp_" + date.getTime()
+        });
     }
 });
 
-app.post('/auth/test/jwt', (req, res)=>
-{
+app.post('/auth/test/jwt', (req, res) => {
     var auth = req.headers.authorization.split(" ");
-    if (auth[0] == "Bearer"){
+    if (auth[0] == "Bearer") {
         var token = auth[1].split("_");
         var timeStamp = new Date().getTime();
         // token expire after 5'
-        if (Number(token[1]) + 5*60 <timeStamp){
-            res.status(200).json({"result":"success"});
-        }else{
-            res.status(201).json({"result":"Failed authentication"});
+        if (Number(token[1]) + 5 * 60 < timeStamp) {
+            res.status(200).json({
+                "result": "success"
+            });
+        } else {
+            res.status(201).json({
+                "result": "Failed authentication"
+            });
+        }
+    }
+});
+
+
+app.get('/auth/basic', (req, res) => {
+    getCredential();
+    var auth = req.headers.authorization.split(" ");
+    if (auth[0] == "Basic") {
+        let buff = new Buffer(auth[1], 'base64');
+        let decodedAuth = buff.toString('ascii');
+        if (decodedAuth == (username + ":" + password)) {
+            res.status(200).json({
+                "result": "success"
+            });
+        } else {
+            res.status(201).json({
+                "result": "Failed Authentication"
+            });
         }
     }
 });
